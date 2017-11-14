33a571afad85f9ac356db6ddeb875960b11d803e
masahiro.oshita <minddnim@gmail.com>
2017-11-14 22:15:52 +0900

    P18まで

--- ch02/02.R
+++ ch02/02.R
@@ -1,3 +1,5 @@
+library(plyr)
+
 # 先頭データの表示
 head(airquality)
 
@@ -63,3 +65,53 @@ b <- data.frame(id=c(1,3,5,11), id2=c(11, 17, 15, 24), y=c(100, 200, 150, 400))
 
 # 全部のデータを残す結合 (full join)
 (ab4 <- merge(a, b, by="id", all=T))
+
+# join 関数群は, キーとなる変数の方が厳密に同じでなければならないため、integerで指定してデータを作成
+a <- data.frame(id=as.integer(1:10), id2=as.integer(11:20), x = 101:110)
+
+b <- data.frame(id=as.integer(c(1,3,5,11)), id2=as.integer(c(11, 17, 15, 24)), y=c(100, 200, 150, 400))
+
+# 内部結合
+ab1 <- inner_join(a, b, by="id")
+
+# 表示
+ab1
+
+ab1_2 <- inner_join(a, b, by=c("id", "id2"))
+
+ab1_2
+
+ab2 <- left_join(a, b, by="id", all.x=T)
+
+ab2
+
+library(reshape2)
+head(airquality)
+
+# meltを使って、MonthとDayをキーにして列名と値を列挙する
+airquality.melt <- melt(airquality, id=c("Month", "Day"))
+head(airquality.melt)
+
+# variableとvalueの列名を定義
+airquality.melt <- melt(airquality, id=c("Month", "Day"), variable.name="air", value.name="val")
+head(airquality.melt)
+
+# サンプル用のデータセットの作成
+aqm <- melt(airquality, id = c("Month", "Day"), na.rm = TRUE)
+
+# 各月の平均値を出力
+dcast(aqm, Month ~ variable, mean)
+
+dcast(aqm, Month ~ variable, mean, margins = "Month")
+
+dcast(aqm, Month ~ variable, mean, margins = "variable")
+
+dcast(aqm, Month ~ variable, mean, margins = c("Month", "variable"))
+
+dcast(aqm, Month ~ variable, mean, margins = T)
+
+dcast(aqm, Month ~ variable, mean, subset = .(variable == c("Ozone", "Temp")))
+
+head(dcast(aqm, Month + Day ~ variable, mean, subset = .(variable == c("Ozone", "Temp"))))
+
+head(dcast(aqm, Month ~ variable + Day, mean, subset = .(variable == c("Temp"))), n = 3)
